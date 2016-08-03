5b1af9bb5a6b715e3c43a594173ef6a9b29d798e
Mark Lyck <mark.lyck@gmail.com>
2016-08-03 12:34:11 -0500

    Basic header done

--- app/scripts/components/Header.js
+++ app/scripts/components/Header.js
@@ -1,15 +1,38 @@
 import React from 'react'
 import {Link} from 'react-router'
 
+import store from '../store'
+
 const Header = React.createClass({
+  getInitialState: function() {
+    return {authtoken: store.session.get('authtoken')}
+  },
+  componentDidMount: function() {
+    store.session.on('change', this.updateHeader)
+  },
+  updateHeader: function() {
+    this.setState({authtoken: store.session.get('authtoken')})
+  },
   render: function() {
+    let navLinks = (
+      <nav>
+        <Link to="login">Login</Link>
+        <Link to="signup">Signup</Link>
+      </nav>
+    )
+
+    if (this.state.authtoken || localStorage.authtoken) {
+      navLinks = (
+        <nav>
+          <a href="#">Logout</a>
+        </nav>
+      )
+    }
+
     return (
       <header>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="login">Login</Link>
-          <Link to="signup">Signup</Link>
-        </nav>
+        <Link to="/" id="logo">BringTheBand</Link>
+        {navLinks}
       </header>
     )
   }
